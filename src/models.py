76da2bfa3ee80f91acb3caa76aadefd3e29d5430
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-08 19:25:49 +0000

    planet table added

--- src/models.py
+++ src/models.py
@@ -25,11 +25,35 @@ class Character(db.Model):
     name = db.Column(db.String(50), nullable=False)
     heigth = db.Column(db.Integer(4), nullable=True)
     mass = db.Column(db.Integer(4), nullable=True)
-    hair_color = db, Column(db.String(10), nullable=True)
-    skin_color = db, Column(db.String(10), nullable=True)
-    eye_color = db, Column(db.String(10), nullable=True)
-    birth_year = db, Column(db.String(10), nullable=False)
-    gender = db, Column(db.String(10), nullable=True)
+    hair_color = db.Column(db.String(10), nullable=True)
+    skin_color = db.Column(db.String(10), nullable=True)
+    eye_color = db.Column(db.String(10), nullable=True)
+    birth_year = db.Column(db.String(10), nullable=False)
+    gender = db.Column(db.String(10), nullable=True)
+    planet_id = db.Column(db.Integer(), db.ForeignKey("Planet.id"))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    rotation_period = db.Column(db.Integer(3), nullable=False)
+    orbital_period = db.Column(db.Integer(4), nullable=False)
+    diameter = db.Column(db.Integer(5), nullable=False)
+    climate = db.Column(db.String(10), nullable=False)
+    gravity = db.Column(db.String(10), nullable=False)
+    terrain = db.Column(db.String(100), nullable=False)
+    surface_water = db.Column(db.Integer(4), nullable=False)
+    population = db.Column(db.Integer(10), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
