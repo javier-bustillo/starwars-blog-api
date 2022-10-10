7d9748682e332c23ba365a4ce6c94d5eb9f274b5
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-10 09:29:58 +0000

    working on relationships

--- src/models.py
+++ src/models.py
@@ -23,14 +23,14 @@ class User(db.Model):
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    heigth = db.Column(db.Integer(4), nullable=True)
-    mass = db.Column(db.Integer(4), nullable=True)
+    heigth = db.Column(db.Integer(), nullable=True)
+    mass = db.Column(db.Integer(), nullable=True)
     hair_color = db.Column(db.String(10), nullable=True)
     skin_color = db.Column(db.String(10), nullable=True)
     eye_color = db.Column(db.String(10), nullable=True)
     birth_year = db.Column(db.String(10), nullable=False)
     gender = db.Column(db.String(10), nullable=True)
-    planet_id = db.Column(db.Integer(), db.ForeignKey("Planet.id"))
+    planets = db.relationship('Planet', backref='character', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -46,14 +46,16 @@ class Character(db.Model):
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    rotation_period = db.Column(db.Integer(3), nullable=False)
-    orbital_period = db.Column(db.Integer(4), nullable=False)
-    diameter = db.Column(db.Integer(5), nullable=False)
+    rotation_period = db.Column(db.Integer(), nullable=False)
+    orbital_period = db.Column(db.Integer(), nullable=False)
+    diameter = db.Column(db.Integer(), nullable=False)
     climate = db.Column(db.String(10), nullable=False)
     gravity = db.Column(db.String(10), nullable=False)
     terrain = db.Column(db.String(100), nullable=False)
-    surface_water = db.Column(db.Integer(4), nullable=False)
-    population = db.Column(db.Integer(10), nullable=False)
+    surface_water = db.Column(db.Integer(), nullable=False)
+    population = db.Column(db.Integer(), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey(
+        "character.id"), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
